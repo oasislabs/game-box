8db42ca8b67db72fb1406734e79f2d1d8ee6bcf5
Andrew Osheroff <andrewosh@gmail.com>
2018-12-10 19:32:29 -0800

    Update dep names and paths

--- test/test-server.js
+++ test/test-server.js
@@ -1,6 +1,6 @@
 const Web3 = require('web3');
 const minimist = require('minimist')
-const { GameServer, Game } = require('game-client')
+const { GameServer, Game } = require('oasis-game-client')
 
 const GameServerContract = artifacts.require('GameServerContract')
 const web3 = new Web3(GameServerContract.web3.currentProvider)

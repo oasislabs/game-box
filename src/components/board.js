8d6f4e5ae8ab0c41ad97068b044d1d3e49d31bf3
Andrew Osheroff <andrewosh@gmail.com>
2018-12-10 23:18:03 -0800

    Bug fixes

--- src/components/board.js
+++ src/components/board.js
@@ -8,7 +8,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { GameInfo } from 'game-components';
+import { GameInfo } from 'oasis-game-components';
 import './board.css';
 
 class Board extends React.Component {

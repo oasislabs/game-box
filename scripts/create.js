8875b4efd58b0d2ac5619a038ce1f122ec2fda69
Andrew Osheroff <andrewosh@gmail.com>
2019-01-04 17:58:00 -0800

    Add seed argument to game creation script

--- scripts/create.js
+++ scripts/create.js
@@ -7,7 +7,9 @@ const { Game, GameServer } = require('oasis-game-client')
 
 const truffleConfig = require('../truffle-config.js')
 
-let args = minimist(process.argv.slice(2))
+let args = minimist(process.argv.slice(2), {
+  string: ['bots', 'players']
+})
 
 module.exports = async function (cb) {
   let network = args.network || 'development'
@@ -16,7 +18,21 @@ module.exports = async function (cb) {
   let eventsWeb3c = new Web3c(new Web3.providers.WebsocketProvider(networkConfig.wsEndpoint))
 
   let serverAddress = args.server || artifacts.require('GameServerContract').address
-  let players = args.players.split(',')
+  let players = args.players ? args.players.split(',') : []
+  let bots = args.bots ? args.bots.split(',') : []
+  let seed = args.seed || 1
+
+  let playerArgs = [...players.map(address => {
+    return {
+      address,
+      is_bot: false
+    }
+  }), ...bots.map(address => {
+    return {
+      address,
+      is_bot: true
+    }
+  })]
 
   let spinner = ora({
     text: chalk.blue(`Creating a new game with game contract ${serverAddress}`),
@@ -30,12 +46,7 @@ module.exports = async function (cb) {
   })
 
   try {
-    let game = await server.createGame(players.map(address => {
-      return {
-        address,
-        is_bot: false
-      }
-    }))
+    let game = await server.createGame(playerArgs, seed)
     await game.ready()
     spinner.succeed(chalk.green(`Created a new game with ID: ${game.id}`))
   } catch (err) {
